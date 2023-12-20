5fee7a230ff3bc4d55402b5d05defc93016a0237
alin <alinmansita@gmail.com>
2023-12-20 16:47:03 +0100

    gestion de connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Security;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -20,11 +21,14 @@ class SecurityController extends AbstractController
             $welcomeMessage = "Bienvenue : " . implode(', ', $userRoles);
             $this->addFlash('success', $welcomeMessage);
 
-            return $this->redirectToRoute('login/login.html.twig'); // Ajustez le nom de la route selon vos besoins
+            // Rediriger vers 'user_list' si l'utilisateur est connecté
+            return $this->redirectToRoute('app_userliste', ['firstname' => $this->getUser()->getFirstname()]);
+
         }
 
         return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,44 +13,39 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use App\Repository\UserRepository;
-
-
+use Symfony\Component\Security\Core\Security;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(Request $request, UserPasswordHasherInterface $userPasswordHasher,EntityManagerInterface $entityManager,SessionInterface $session): Response
-    //objet  $request pour les request, instance em=pour interagir avec la bdd
-    {   
-            // on a ajouté pour la sseion
+    public function index(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SessionInterface $session, Security $security): Response
+    {
         $session->set('user', $this->getUser());
-        //objet $user contenant les données de l'utilisateur fournis par le formulaire
         $user = new User();
-        //création du formulaire avc la class type de formulaire UserFormType et l'associe à l'objet $user. Le formulaire est ensuite utilisé pour gérer les données d'entrée de l'utilisateur.
         $form = $this->createForm(UserFormType::class, $user);
-        // raite la soumission du formulaire. 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-    // encode the plain password
-        $user->setPlainPassword(
-            $userPasswordHasher->hashPassword(
-                $user,
-                $form->get('plainPassword')->getData()
-            )
-        );
-    
-        $entityManager->persist($user);
-        $entityManager->flush();
-        // Rediriger vers la page de connexion après l'inscription
-        return $this->redirectToRoute('app_login');
-    }
-    
-        return $this->render('user/user.html.twig', [
-        'form' => $form->createView(),
-    ]);
+            $user->setPlainPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+                // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+                return $this->redirectToRoute('app_login');
+            }
+    
+
+        return $this->render('user/user.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
+
 
     
     #[Route('/userliste', name: 'app_userliste')]
