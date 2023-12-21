c46220ea214a489039b0163bd6861ed72d5c1d53
alin <alinmansita@gmail.com>
2023-12-21 15:15:53 +0100

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,23 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Permis;
+use App\Form\PermisFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PermisRepository;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(PermisRepository $permisRepository): Response
+    {  
+        $permis = $permisRepository->findAll();
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+             'permis' => $permis,
         ]);
     }
 }
